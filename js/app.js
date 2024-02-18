52c766d13917af28ea27bc0a933f92c471c4447b
Daniel Lucas <daniel.lucas1507@gmail.com>
2024-02-18 12:52:47 -0300

    feat: remover despesas e filtrar despesas.

--- js/app.js
+++ js/app.js
@@ -38,17 +38,59 @@ class Bd {
     }
 
     recuperarRegistros() {
-        let id = localStorage.getItem('id');
         let despesas = [];
+        let id = localStorage.getItem('id');
         for (let i = 1; i <= id; i++) {
             let despesa = JSON.parse(localStorage.getItem(i));
-            if (i === null) {
+            if (despesa === null) {
                 continue;
             }
+            despesa.id = i;
             despesas.push(despesa);
         }
         return despesas
     }
+
+    pesquisar(despesa) {
+        let despesasFiltradas = [];
+
+        despesasFiltradas = this.recuperarRegistros();
+
+        if (despesa.ano != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+        if (despesa.mes != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+        }
+        if (despesa.dia != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+        }
+        if (despesa.tipo != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+        }
+        if (despesa.descricao != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+        }
+        if (despesa.valor != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+        }
+
+        limpaCampos();
+        return despesasFiltradas;
+    }
+
+    removerDespesa(id){
+        localStorage.removeItem(id);
+    }
+}
+
+function limpaCampos() {
+    document.getElementById('ano').value = '';
+    document.getElementById('mes').value = '';
+    document.getElementById('dia').value = '';
+    document.getElementById('tipo').value = '';
+    document.getElementById('descricao').value = '';
+    document.getElementById('valor').value = '';
 }
 
 let bd = new Bd();
@@ -75,10 +117,12 @@ function cadastrarDespesa() {
 }
 
 
-function carregarListaDespesas() {
-    let despesas = [];
-    despesas = bd.recuperarRegistros();
-    var listaDespesas = document.getElementById('lista_despesas');
+function carregarListaDespesas(despesas = []) {
+
+    if (despesas.length == 0) {
+        despesas = bd.recuperarRegistros();
+    }
+    let listaDespesas = document.getElementById('lista_despesas');
 
     despesas.forEach(function (d) {
         let row = listaDespesas.insertRow();
@@ -88,18 +132,30 @@ function carregarListaDespesas() {
         switch (d.tipo) {
             case '1': d.tipo = 'Alimentação';
                 break;
-            case '2': d.tipo = 'Educação'; 
+            case '2': d.tipo = 'Educação';
                 break;
-            case '3': d.tipo = 'Lazer'; 
+            case '3': d.tipo = 'Lazer';
                 break;
-            case '4': d.tipo = 'Saúde'; 
+            case '4': d.tipo = 'Saúde';
                 break;
-            case '5': d.tipo = 'TRansporte'; 
+            case '5': d.tipo = 'TRansporte';
                 break;
         }
         row.insertCell(1).innerHTML = d.tipo;
         row.insertCell(2).innerHTML = d.descricao;
         row.insertCell(3).innerHTML = d.valor;
+        let btn = document.createElement('button');
+        btn.className = 'btn btn-danger';
+        btn.innerHTML = '<i class="fas fa-times"></i>';  
+        btn.id = `id_despesa${d.id}`;
+        btn.onclick = function() {
+
+            let id = this.id.replace('id_despesa', '');
+
+            bd.removerDespesa(id);
+            window.location.reload();
+        }
+        row.insertCell(4).append(btn);
 
 
     })
@@ -113,6 +169,8 @@ function gravar(despesa) {
         document.getElementById('modal_titulo').innerHTML = 'Registro realizado!';
         document.getElementById('modal_corpo').innerHTML = 'Despesa registrada com sucesso!';
         bd.gravar(despesa);
+        limpaCampos();
+
         var abreModal = new bootstrap.Modal(modal);
         abreModal.show();
     } else {
@@ -123,5 +181,56 @@ function gravar(despesa) {
         var abreModal = new bootstrap.Modal(modal);
         abreModal.show();
     }
+
+
+}
+
+function pesquisarDespesa() {
+    let ano = document.getElementById('ano').value;
+    let mes = document.getElementById('mes').value;
+    let dia = document.getElementById('dia').value;
+    let tipo = document.getElementById('tipo').value;
+    let descricao = document.getElementById('descricao').value;
+    let valor = document.getElementById('valor').value;
+
+    let despesa = new Despesa(
+        ano,
+        mes,
+        dia,
+        tipo,
+        descricao,
+        valor
+    )
+
+    let despesas = bd.pesquisar(despesa);
+
+    let listaDespesas = document.getElementById('lista_despesas');
+    listaDespesas.innerHTML = '';
+    console.log(despesas);
+    despesas.forEach(function (d) {
+        let row = listaDespesas.insertRow();
+
+        row.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+
+        switch (d.tipo) {
+            case '1': d.tipo = 'Alimentação';
+                break;
+            case '2': d.tipo = 'Educação';
+                break;
+            case '3': d.tipo = 'Lazer';
+                break;
+            case '4': d.tipo = 'Saúde';
+                break;
+            case '5': d.tipo = 'TRansporte';
+                break;
+        }
+        row.insertCell(1).innerHTML = d.tipo;
+        row.insertCell(2).innerHTML = d.descricao;
+        row.insertCell(3).innerHTML = d.valor;
+
+        
+
+
+    })
 
 }
